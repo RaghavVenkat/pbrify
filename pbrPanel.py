ccb23bf8a8c048adce06d3d8aaf187abc1e9cfc3
RaghavVenkat <raghav16697@gmail.com>
2019-06-14 11:22:11 +0530

    normal heights handler postponed

--- pbrPanel.py
+++ pbrPanel.py
@@ -4,7 +4,7 @@ from bpy.props import BoolProperty
 
 # Custom Properties
 bpy.types.Object.mapNodes = BoolProperty(name="mapnode", description="Attribute Replacer Flag")
-bpy.types.Object.normalHeights = BoolProperty(name="nomralheights", description="Normal and Heightmap Flag")
+#bpy.types.Object.normalHeights = BoolProperty(name="nomralheights", description="Normal and Heightmap Flag")
 bpy.types.Object.levelControllers = BoolProperty(name="levelcontrollers", description="Adds Brightness and Contrast Nodes for all non color data maps")
 #bpy.types.Object.invertGloss = BoolProperty(name="invertgloss", description="Adds Invert node to support Glossy Maps")
 bpy.types.Object.materialName = bpy.props.StringProperty(name = "PBR Material Name", description = "My description",default = "default")
@@ -24,7 +24,7 @@ class PbrifyInterface(bpy.types.Panel):
         layout = self.layout
         currentObject = context.object
         layout.label(text='Advance PBR Material Settings', text_ctxt='', translate=True, icon='NONE', icon_value=0)
-        layout.prop(currentObject,'normalHeights',text='Normal + Height Map (Bumps)')
+        #layout.prop(currentObject,'normalHeights',text='Normal + Height Map (Bumps)')
         layout.prop(currentObject,'mapNodes',text='Use Attribute Nodes instead of Mapping Nodes') 
         layout.prop(currentObject,'levelControllers',text='Add Brightness/Contrast Controllers') 
         #layout.prop(currentObject,"invertGloss",text='Invert Glossiness Map') 
