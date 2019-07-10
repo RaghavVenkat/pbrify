aabec830222803301cd6d775b541208de3dc57ac
Raghav Venkat <raghav16697@gmail.com>
2019-07-10 19:50:20 +0530

    Cycles auto switch resolved

--- pbrOp.py
+++ pbrOp.py
@@ -8,14 +8,14 @@ class PbrifyCreate(bpy.types.Operator):
 
     def execute(self, context):
         
-        # Check engine mode 
+ # Check engine mode 
         if(bpy.context.scene.render.engine == 'EEVEE'):
-            bpy.context.scene.render.engine = 'CYCLES'
-		elif(bpy.context.scene.render.engine == 'EEVEE'):
-			bpy.context.scene.render.engine = 'CYCLES'
-		else
-			print('Please use EEVEE or CYCLES engine')
-			return {'CANCELLED'}
+            bpy.context.scene.render.engine = 'EEVEE'
+	elif(bpy.context.scene.render.engine == 'CYCLES'):
+	    bpy.context.scene.render.engine = 'CYCLES'
+	else
+	    print('Please use EEVEE or CYCLES engine')
+	    return {'CANCELLED'}
 			
 
         # Check object type
