cadd3769d2fb76d9d7021bbf98ce33fdb89108c6
Raghav Venkat <raghav16697@gmail.com>
2019-07-10 19:58:07 +0530

    Engine conflicts resolved

--- release/pbrify.py
+++ release/pbrify.py
@@ -41,13 +41,13 @@ class PbrifyCreate(bpy.types.Operator):
     def execute(self, context):
         
         # Check engine mode 
-        if(bpy.context.scene.render.engine == 'EEVEE'):
-            bpy.context.scene.render.engine = 'EEVEE'
-	elif(bpy.context.scene.render.engine == 'CYCLES'):
-	    bpy.context.scene.render.engine = 'CYCLES'
-	else
-	    print('Please use EEVEE or CYCLES engine')
-	    return {'CANCELLED'}
+        if(bpy.context.scene.render.engine == 'BLENDER_EEVEE'):
+            bpy.context.scene.render.engine = 'BLENDER_EEVEE'
+        elif(bpy.context.scene.render.engine == 'CYCLES'):
+            bpy.context.scene.render.engine = 'CYCLES'
+        else:
+            print('Please use EEVEE or CYCLES engine')
+            return {'CANCELLED'}
 			
 
         # Check object type
